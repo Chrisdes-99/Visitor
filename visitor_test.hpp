19b1d04cb0f62ac65e23f8fe66cfb0f8aae83be2
kobe-cb <kobecbr@gmail.com>
2020-12-01 12:55:24 -0800

    Added Additional Tests

--- visitor_test.hpp
+++ visitor_test.hpp
@@ -22,7 +22,7 @@ TEST(BinaryTest, AddTest) {
 
 }
 
-TEST(BinaryTest_Sub, SubTest) {
+TEST(BinaryTest, SubTest) {
     Op* ten = new Op(10);
     Op* five = new Op(5);
 
@@ -38,7 +38,7 @@ TEST(BinaryTest_Sub, SubTest) {
 
 }
 
-TEST(BinaryTest_Mult, MultTest) {
+TEST(BinaryTest, MultTest) {
     Op* six = new Op(6);
     Op* twelve = new Op(12);
 
@@ -55,7 +55,7 @@ TEST(BinaryTest_Mult, MultTest) {
 
 }
 
-TEST(BinaryTest_Div, DivTest) {
+TEST(BinaryTest, DivTest) {
     Op* ten = new Op(10);
     Op* five = new Op(5);
 
@@ -82,7 +82,153 @@ TEST(NullTest, OpTest) {
 }
 
 
+TEST(VisitorTest, MultTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
 
- 
+    Mult* temp = new Mult(num1, num2);
+
+    Base* dummy = new Add(temp, num1);
+
+    Iterator* it = new PreorderIterator(dummy); //dummy->create_iterator();
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done()) {
+        it->current()->accept(visit);
+        it->next();
+    }
+    EXPECT_EQ(visit->op_count(), 3);
+    EXPECT_EQ(visit->mult_count(), 1);
+    EXPECT_EQ(visit->sub_count(), 0);
+    EXPECT_EQ(visit->add_count(), 0);
+}
+
+TEST(VisitorTest, AddTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
+
+    Add* temp = new Add(num1, num2);
+
+    Base* dummy = new Add(temp, num1);
+
+    Iterator* it = new PreorderIterator(dummy); //dummy->create_iterator();
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done()) {
+        it->current()->accept(visit);
+        it->next();
+    }
+    EXPECT_EQ(visit->op_count(), 3);
+    EXPECT_EQ(visit->mult_count(), 0);
+    EXPECT_EQ(visit->sub_count(), 0);
+    EXPECT_EQ(visit->add_count(), 1);
+}
+
+TEST(VisitorTest, SubTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
+
+    Sub* temp = new Sub(num1, num2);
+
+    Base* dummy = new Add(temp, num1);
+
+    Iterator* it = new PreorderIterator(dummy); //dummy->create_iterator();
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done()) {
+        it->current()->accept(visit);
+        it->next();
+    }
+    EXPECT_EQ(visit->op_count(), 3);
+    EXPECT_EQ(visit->mult_count(), 0);
+    EXPECT_EQ(visit->sub_count(), 1);
+    EXPECT_EQ(visit->add_count(), 0);
+}
+
+TEST(VisitorTest, DivTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
+
+    Div* temp = new Div(num1, num2);
+
+    Base* dummy = new Add(temp, num1);
+
+    Iterator* it = new PreorderIterator(dummy); //dummy->create_iterator();
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done()) {
+        it->current()->accept(visit);
+        it->next();
+    }
+    EXPECT_EQ(visit->op_count(), 3);
+    EXPECT_EQ(visit->div_count(), 1);
+    EXPECT_EQ(visit->sub_count(), 0);
+    EXPECT_EQ(visit->add_count(), 0);
+}
+
+TEST(VisitorTest, PowTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
+
+    Pow* temp = new Pow(num1, num2);
+
+    Base* dummy = new Add(temp, num1);
+
+    Iterator* it = new PreorderIterator(dummy); // dummy->create_iterator();
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done()) {
+        it->current()->accept(visit);
+        it->next();
+    }
+    EXPECT_EQ(visit->op_count(), 3);
+    EXPECT_EQ(visit->pow_count(), 1);
+    EXPECT_EQ(visit->sub_count(), 0);
+    EXPECT_EQ(visit->add_count(), 0);
+}
+
+TEST(VisitorTest, BigTreeTest) {
+    Base* num1 = new Op(3);
+    Base* num2 = new Op(7);
+
+    Pow* pow = new Pow(num1, num2);
+    Mult* mult = new Mult(pow, num1);
+    Add* add = new Add(mult, num2);
+    Sub* test = new Sub(add, num1);
+
+    Base* dummy = new Add(test, num1);
+
+    Iterator* it = new PreorderIterator(dummy);
+    CountVisitor* visit = new CountVisitor();
+
+    it->first();
+    while (!it->is_done())  {
+        it->current()->accept(visit);
+        it->next();
+    }
+
+    EXPECT_EQ(visit->op_count(), 6);
+    EXPECT_EQ(visit->pow_count(), 1);
+    EXPECT_EQ(visit->mult_count(), 1);
+    EXPECT_EQ(visit->add_count(), 1);
+    EXPECT_EQ(visit->sub_count(), 1);
+    EXPECT_EQ(visit->div_count(), 0);
+}
+
+
+
+
+
+
+
+
+
+
+
    
 #endif //__VISITOR_TEST_HPP__
